d2a25b97d1dbba6950bf63386e875987e1981704
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-10-06 10:29:16 +0300

    ✨ Deploy GovernanceToken

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,8 +1,28 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types/runtime"
 import { DeployFunction } from "hardhat-deploy/types"
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
+import verify from "../utils/verify"
 
-const deployGovernanceToken: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-    console.log("Haist kotarache")
+const deployGovernanceToken: DeployFunction = async ({
+    getNamedAccounts,
+    deployments,
+    network,
+}: HardhatRuntimeEnvironment) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    log("Deploying Governance Token...")
+
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: networkConfig[network.name]?.blockConfirmations || 1,
+    })
+
+    log(`GovernanceToken at ${governanceToken.address}`)
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(governanceToken.address, [])
+    }
 }
 
 export default deployGovernanceToken
